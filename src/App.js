a61ffe4be3f0993f1b0a52aaf7f22d37df984eb1
bharti zade <bhartigzade@gmail.com>
2023-04-14 23:08:37 +0530

    routing14apr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Update import to include 'Routes'
+import Home from './components/Home';
+import Contact from './components/Contact';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes> {/* Wrap Routes component around Route components */}
+        <Route path="/" element={<Home />} /> {/* Use 'element' prop instead of 'component' */}
+        <Route path="/contact" element={<Contact />} /> {/* Use 'element' prop instead of 'component' */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>Welcome to the Home Page!</h1>;
+};
+
+export default Home;
+
+
+// Create a simple React app with React Router. Set up two routes: one for the home page and one for a contact page. Use React Router's Link component to navigate between the two pages.
+
+// 2. Build a multi-page app with React Router that allows users to create, edit, and delete notes. Use React Router's Route component to set up the routes for each page. Use the Link component to navigate between the pages
